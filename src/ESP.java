740ed0e3bbae1ba70919082120ea41f1c5a649e9
Darrel Amoah-Duah <137457043+SomeoneAD7@users.noreply.github.com>
2026-03-18 12:27:14 +0000

    these are the files as at 18th March,2026;12:29pm.

--- src/ESP.java
+++ src/ESP.java
@@ -5,14 +5,27 @@ public class ESP{
         String[] colours = {"Red", "Green", "Blue", "Orange",  "Yellow"}; 
         Scanner myinput = new Scanner(System.in);
         Random rand = new Random();
+        int correct = 0; 
 
-        int randNum = rand.nextInt(5); // (0-4) 
+        int randNum = rand.nextInt(5);  // (0-4) 
 
-        System.out.print("Welcome to the ESP Game!");
+        System.out.println("Welcome to the ESP Game!");
         
         for(int i = 1; i <= 10; i++){
             System.out.print("Guess the colour : "); 
-            
-        }
+            String userGuess = myinput.nextLine(); 
+
+            if (userGuess == colours[randNum]){
+                correct++; 
+                System.out.println("You guessed correctly!"); 
+                System.out.println("The correct colour was " + colours[randNum] + "."); 
+            } 
+            else{
+                System.out.println("Wrong! Try again."); 
+                System.out.println("The correct colour was " + colours[randNum] + "."); 
+            }            
+        } 
+
+        System.out.println("You guessed the right colour " + correct + " times."); 
     }
 }
